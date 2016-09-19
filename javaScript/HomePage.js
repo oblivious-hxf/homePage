a544399292117752eb40b4364b5eeb5acc0bbeff
oblivious_hxf <oblivious_hxf@outlook.com>
2016-09-19 21:55:58 +0800

    new page
    = =

--- javaScript/HomePage.js
+++ javaScript/HomePage.js
@@ -1,108 +1,11 @@
-function skinListShow(){
-	document.getElementById('skinBox').style.visibility='visible';
-}
+$(document).ready(function () {
 
-function skinListHidden(){
-	document.getElementById('skinBox').style.visibility='hidden';
-}
+            $('li.mainlevel').mousemove(function () {
+                $(this).find('ul').slideDown("1000"); //you can give it a speed
+            });
+            $('li.mainlevel').mouseleave(function () {
+                $(this).find('ul').slideUp("fast");
+            });
 
-function fontListShow(){
-	document.getElementById('fontBox').style.visibility='visible';
-}
-
-function fontListHidden(){
-	document.getElementById('fontBox').style.visibility='hidden';
-}
-
-function allHidden(){
-    document.getElementById('skinBox').style.visibility='hidden';
-	document.getElementById('fontBox').style.visibility='hidden';
-}
-
-
-function DefaultSkinStyle(){
-	document.getElementById('slidebarid').style.background="#FFCC80 url(file:///E:/HTML/myHomePage/image/defaultSkin.png) repeat-y";
-	document.getElementById('public-header-id').style.background="#FFCC80 url(file:///E:/HTML/myHomePage/image/defaultSkin.png) repeat-x";
-}
-
-function GreenSkinStyle(){
-	document.getElementById('slidebarid').style.background="#FFCC80 url(file:///E:/HTML/myHomePage/image/greenSkin.png) repeat-y";
-	document.getElementById('public-header-id').style.background="#FFCC80 url(file:///E:/HTML/myHomePage/image/greenSkin.png) repeat-x";
-}
-
-
-function treeSkinStyle(){
-	document.getElementById('slidebarid').style.background="#FFCC80 url(file:///E:/HTML/myHomePage/image/treeSkin.png) repeat-y";
-	document.getElementById('public-header-id').style.background="#FFCC80 url(file:///E:/HTML/myHomePage/image/treeSkin.png) repeat-x";
-}
-
-function coolBlackSkinStyle(){
-	document.getElementById('slidebarid').style.background="#FFCC80 url(file:///E:/HTML/myHomePage/image/coolBlackSkin.png) repeat-y";
-	document.getElementById('public-header-id').style.background="#FFCC80 url(file:///E:/HTML/myHomePage/image/coolBlackSkin.png) repeat-x";
-}
-
-
-function defaultFontStyle(){
-	document.body.style.fontFamily="SimSun";
-}
-
-function songtiFontStyle(){
-	document.body.style.fontFamily="STFangsong";
-}
-
-function kaishuFontStyle(){
-	document.body.style.fontFamily="KaiTi";
-}
-
-function lishuFontStyle(){
-	document.body.style.fontFamily="LiSu";
-}
-
-function openCnBing(){
-	document.getElementById("mainFrame").src="http://cn.bing.com/";
-}
-
-function openBing(){
-	document.getElementById("mainFrame").src="http://global.bing.com/?FORM=HPCNEN&setmkt=en-us&setlang=en-us";
-}
-
-function openGoogle(){
-	document.getElementById("mainFrame").src="https://www.google.com/";
-}
-
-function openZhihu(){
-	document.getElementById("mainFrame").src="http://zhihu.sogou.com/";
-}
-
-function openBilibili(){
-	document.getElementById("mainFrame").src="http://www.bilibili.com/index.html";
-}
-
-function openWeibo(){
-	document.getElementById("mainFrame").src="http://weibo.com/";
-}
-
-function openYoudao(){
-	document.getElementById("mainFrame").src="http://dict.youdao.com/?keyfrom=cidian";
-}
-
-function openTaobao(){
-	document.getElementById("mainFrame").src="https://www.taobao.com/";
-}
-
-function openJingdong(){
-	document.getElementById("mainFrame").src="http://www.jd.com/";
-}
-
-function openWP7(){
-	document.getElementById("mainFrame").src="http://tieba.baidu.com/f?kw=wp7&fr=index";
-}
-
-function openWangyi(){
-	document.getElementById("mainFrame").src="http://news.163.com/";
-}
-
-function openTiedaobu(){
-	document.getElementById("mainFrame").src="http://www.12306.cn/mormhweb/";
-}
+        });
 
